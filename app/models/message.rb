a42e5b877bdd0c6a285364505ac9ca3714cae82d
magic2k <magic2ktech@gmail.com>
2014-04-29 18:45:35 +0400

    messages and conversations models and controllers WIP

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
   belongs_to :conversation
 
-  validates_presence_of :content
+  # validates_presence_of :content, :conversation_id
   validates_length_of :content, maximum: 250
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+3.times do |i|
+  i+=1
+
+  usr = User.create!(
+            firstname: "Test#{i}",
+            lastname: 'Testovich',
+    city: 'Tristram',
+    country: 'Tamriel',
+    skype: 'Aloha666',
+    birthdate: '24-11-1990',
+    phone: '(812)555-55-55',
+    vkontakte: 'ololo.vk.com',
+    odkl:     'leha-ujeban.odkl.ru',
+    twitter:  'twitter.com/huitterr',
+    admin:    'f',
+    usr_type: 'showman',
+    banned:   'f',
+    completed: 't',
+    about:    'Ololooooo trolooloooooo. Thats all.',
+    email:    "dorkpork#{i}@morpork.com",
+    # encrypted_password: '$2a$10$oAHfisCQao5937fNz2EHXOvPx/5sCpGPkDu7nzwKOyCEsow28jzzC',
+    created_at: '2014-04-19 08:45:13.628063',
+    updated_at: '2014-04-19 08:45:16.055981',
+    # authentication_token: '7xdjzkk8y8fWm9ByA9-j',
+    password: 'huiyoooooddd'
+)
+
+typ = ShowmanType.create!(user_id: i, bartender:true)
+
+end
